db50dd51821e5ddaddba767620c60eff5ec5828e
Peter Mihal <petermihal.mail@gmail.com>
2023-11-25 15:08:53 +0100

    change password rules

--- Program.cs
+++ Program.cs
@@ -16,11 +16,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
-    options.AddSecurityDefinition("oauth2", new Microsoft.OpenApi.Models.OpenApiSecurityScheme 
+    options.AddSecurityDefinition("oauth2", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
     {
-        In = ParameterLocation.Header, 
-        Name = "Authorization", 
-        Type = SecuritySchemeType.ApiKey 
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
     });
     options.OperationFilter<SecurityRequirementsOperationFilter>();
 });
@@ -30,8 +30,15 @@ builder.Services.AddDbContext<DataContext>(options =>
 //  opt.UseInMemoryDatabase("TestDb"));
 
 builder.Services.AddAuthorization();
-builder.Services.AddIdentityApiEndpoints<IdentityUser>()
-    .AddEntityFrameworkStores<DataContext>();
+builder.Services.AddIdentityApiEndpoints<IdentityUser>(options =>
+{
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 4;
+    options.Password.RequiredUniqueChars = 0;
+}).AddEntityFrameworkStores<DataContext>();
 
 
 var app = builder.Build();
